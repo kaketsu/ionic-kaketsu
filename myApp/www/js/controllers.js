4e30905ee129847855e4b20c4ceb305112b958d5
kaketsu <huajiewuxi@gmail.com>
2015-09-15 16:16:42 +0800

    20150915001

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,6 +1,40 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope,$state,$timeout) {
+
+    $scope.items = [
+      { id:0, name:'a'},
+      { id:1, name:'b'},
+      { id:2, name:'c'}
+    ]
+
+
+    $scope.doRefresh = function(){
+      
+      console.log('b');
+      loadData(true);        
+
+    }
+
+    function loadData(onRefresh){    //need more study how to loadData
+
+      $timeout( function(){
+
+        console.log('a');
+
+        $scope.$broadcast('scroll.refreshComplete');
+        // 广播一个信号来确定刷新结束
+
+      }, 2000);
+
+    }
+
+    $scope.onClickItem = function(item){
+
+      $state.go('tab.userDetail');
+    }
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
@@ -29,4 +63,14 @@ angular.module('starter.controllers', [])
 
 .controller('MapCtrl', function($scope) {
     
+})
+
+
+.controller('UserDetailCtrl', function($scope) {
+    
+
+    $scope.onClickSwitch = function(index){
+
+        
+    }
 });
--- myApp/www/js/route.js
+++ myApp/www/js/route.js
@@ -5,23 +5,8 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives', 'starter.services'])
+angular.module('starter.route', [])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
 
 .config(function($stateProvider, $urlRouterProvider) {
 
@@ -85,6 +70,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives',
       'tab-map': {
         templateUrl: 'templates/tab-map.html',
         controller: 'MapCtrl'
+      }
+    }
+  })
+
+   .state('tab.userDetail', {
+    url: '/userDetail',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/userDetail.html',
+        controller: 'UserDetailCtrl'
       }
     }
   });
